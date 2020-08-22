10a9d2494eb4723b6c1fea498c848a29410a527f
Andres Alba <bryanalba92@gmail.com>
2020-08-21 23:29:01 -0500

    Practice useState

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import { CounterApp } from './components/01-useState/CounterApp';
+import { CounterWithCustomhook } from './components/01-useState/CounterWithCustomhook';
+// import { HookApp } from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.render(<CounterWithCustomhook />, document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
