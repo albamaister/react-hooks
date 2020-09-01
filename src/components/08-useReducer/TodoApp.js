04c5f20155888275b206a83be794ccf28431627a
Andres Alba <bryanalba92@gmail.com>
2020-08-31 23:46:18 -0500

    useReducer and add todo

---
+++ src/components/08-useReducer/TodoApp.js
@@ -0,0 +1,84 @@
+import React, { useReducer, useEffect } from 'react';
+import { todoReducer } from './todoReducer';
+import {useForm} from '../../hooks/useForm';
+import './styles.css';
+
+
+const init = () => {
+
+    return JSON.parse( localStorage.getItem('todos')) || [];
+ 
+}
+
+export const TodoApp = () => {
+
+    const [todos, dispatch] = useReducer(todoReducer, [], init);
+    const [{description}, handleInputChange, reset] = useForm({
+        description: ''
+    });
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos))
+    }, [todos])
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (description.trim().length <= 1) {
+            return;
+        }
+        const newTodo = {
+            id: new Date().getTime(),
+            desc: description,
+            done: false 
+        };
+        const action = {
+            type: 'add',
+            payload: newTodo
+        }
+        dispatch(action);
+        reset();
+    }
+
+    return (
+        <div>
+            <h1>TodoApp {todos.length}</h1>
+            <div className='row'>
+                <div className='col-7'>
+                    <ul className='list-group list-group-flush'>
+                        { todos.map((todo, index) => (
+                            <li key={todo.id} className='list-group-item'>
+                                <p className='text-center complete'>{index + 1.} {todo.desc}</p>
+                                <button className='btn btn-danger'>
+                                    Borrar
+                                </button>
+                            </li>
+                        ))
+                        }
+                    </ul>
+                </div>
+                <div className='col-5'>
+                    <h4>Agregar TODO</h4>
+                    <hr/>
+                    <form onSubmit={handleSubmit}>
+                        <input
+                            className='form-control'
+                            type='text'
+                            name='description'
+                            placeholder='Aprender...'
+                            autoComplete='off'
+                            value={description}
+                            onChange={handleInputChange}
+                        /> 
+                        <button
+                            type='submit'
+                            className='btn btn-outline-primary mt-1 btn-block'
+                        >
+                            Agregar
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
