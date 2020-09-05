f08ab1690ff15e6d70f48b659a8295e8cc179a1f
Andres Alba <bryanalba92@gmail.com>
2020-09-05 14:46:48 -0500

    CreateContext and useContext

--- src/components/09-useContext/MainApp.js
+++ src/components/09-useContext/MainApp.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { AppRouter } from './AppRouter';
+import { UserContext } from './UserContext';
 
 export const MainApp = () => {
+    const user = {
+        id: 1234,
+        name: 'Andres',
+        email: 'asdas@sfasd.com'
+    }
     return (
         <div>
-            <AppRouter/>
+            <UserContext.Provider value={user}>
+                <AppRouter/>
+            </UserContext.Provider>
         </div>
     )
 }
